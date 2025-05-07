516bd3a7bbb8068e39395cde9a59aedd6e913c9e
wagnerjunr <wagneraparecidojr@gmail.com>
2025-05-07 17:06:53 -0300

    feat/firt-commit

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
